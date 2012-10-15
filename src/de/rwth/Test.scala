df470dce1e71342ee3975dceb1cb4382b46bf6af
DimitrijPlotnikov <dima.plotnikov@gmail.com>
2012-10-15 03:16:30 -0700

    new file

---
+++ src/de/rwth/Test.scala
@@ -0,0 +1,12 @@
+package de.rwth
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: administrator
+ * Date: 10/15/12
+ * Time: 3:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Test {
+
+}
